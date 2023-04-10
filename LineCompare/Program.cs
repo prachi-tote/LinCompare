3c65a24dce45665141ba21e595f18600ad585278
Prachi tote <prachi.tote77@gmail.com>
2023-04-11 00:55:19 +0530

    Calculating length of line

---
+++ LineCompare/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace LineCamparison
+{
+    internal class Program
+    {
+        public static void LengthOfLine()
+        {
+            Console.WriteLine("Enter First Point");
+            Console.Write(" X1 = ");
+            double x1 = double.Parse((Console.ReadLine()));
+            Console.Write(" Y1 = ");
+            double y1 = double.Parse((Console.ReadLine()));
+            Console.WriteLine("Enter Second Point");
+            Console.Write("X2 = ");
+            double x2 = double.Parse((Console.ReadLine()));
+            Console.Write(" Y2 = ");
+            double y2 = double.Parse((Console.ReadLine()));
+            double LenghtOfLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.Write($"Lenght of a Line = {LenghtOfLine}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison");
+            LengthOfLine();
+        }
+    }
+}
